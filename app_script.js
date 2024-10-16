bfc70940a3e6eabd89b7412346eaf317e19cfa1b
wealthsummit <wealthysummit@gmail.com>
2024-10-16 06:10:56 +0200

    Delete app_script.js

--- app_script.js
+++
@@ -1,7 +0,0 @@
-function doPost(e) {
-    var sheet = SpreadsheetApp.openById('YOUR_SHEET_ID').getActiveSheet();
-    var data = JSON.parse(e.postData.contents);
-    sheet.appendRow([data.firstName, data.surname, data.phoneNumber, data.email, data.state]);
-    return ContentService.createTextOutput(JSON.stringify({result: 'success'})).setMimeType(ContentService.MimeType.JSON);
-}
-
